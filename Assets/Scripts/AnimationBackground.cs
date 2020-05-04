b41964bc3b9b31f83e7b99f8720e8d6569f5fae6
Moris <51101778+MOr1s2@users.noreply.github.com>
2020-05-04 11:32:02 +0800

    5/4：完成了基本场景搭建
    完善了背景，可以移动，后期预计会根据玩家的移动来移动；创建了一个基本平台，ground下有两级，第一级是地面，第二级是地面下对应的土，同时为ground添加了碰撞组件

---
+++ Assets/Scripts/AnimationBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBackground : MonoBehaviour
+{
+    Material material;
+    Vector2 movement;
+
+    public Vector2 speed;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        movement += speed * Time.deltaTime;
+        material.mainTextureOffset = movement;
+    }
+}
