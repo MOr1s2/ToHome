d5e28718e0d6e1eb3dcd465852bc98707c4f2600
Moris <51101778+MOr1s2@users.noreply.github.com>
2020-05-20 10:51:23 +0800

    5/20：实现了基本的移动和飞行操作
    暂时以小狐狸为主角，实现了移动飞行，但未添加动画，同时未作地图边界的判定

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed;
+    public float flySpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float faceDirection = Input.GetAxisRaw("Horizontal");
+        float fly = Input.GetAxisRaw("Vertical");
+
+        if (faceDirection != 0)
+        {
+            transform.localScale = new Vector3(faceDirection,1,1);
+        }
+        rb.velocity = new Vector2(horizontalMove * speed, fly * flySpeed);
+    }
+}
