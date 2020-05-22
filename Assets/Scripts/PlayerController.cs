feb02ee0191824de1209adc6a96f70141d27bdb6
guangangning <guangangning@gmail.com>
2020-05-22 17:43:11 +0800

    增加飞、降落、走路“动画

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,29 +17,38 @@ public class PlayerController : MonoBehaviour
     public Text Cherrynum;
     /*加入生命值计数*/
     public int lifevalue;
+    public LayerMask ground;
 
+    Animator animator;
+    Collider2D coll;
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        coll = GetComponent<CircleCollider2D>();  
     }
 
     void FixedUpdate()
     {
         Movement();
+        AnimatorSet();
     }
 
     void Movement()
     {
         float horizontalMove = Input.GetAxis("Horizontal");
         float faceDirection = Input.GetAxisRaw("Horizontal");
-        float fly = Input.GetAxisRaw("Vertical");
+        bool fly = Input.GetButton("Jump");
 
         if (faceDirection != 0)
         {
             transform.localScale = new Vector3(faceDirection,1,1);
+            rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
         }
-        rb.velocity = new Vector2(horizontalMove * speed, fly * flySpeed);
+        if (fly)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, flySpeed);
+        }
     }
 
 
@@ -52,5 +61,15 @@ public class PlayerController : MonoBehaviour
             Cherrycount += 1;
             Cherrynum.text = Cherrycount.ToString();
         }
+    }
+
+    void AnimatorSet()
+    {
+        animator.SetFloat("horizontal_speed",Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("vertical_speed", rb.velocity.y);
+        if (coll.IsTouchingLayers(ground))
+            animator.SetBool("on_floor", true);
+        else
+            animator.SetBool("on_floor", false);
     }
 }
