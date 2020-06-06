739406de706252a42e47a03f847940a7c68608d5
smart-grid2018 <46999312+smart-grid2018@users.noreply.github.com>
2020-06-06 23:29:12 +0800

    新增了在玩家死亡时对各种灭绝鸟类的科普

---
+++ Assets/Scripts/SentenceSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SentenceSpawner : MonoBehaviour
+{
+    public List<GameObject> Sentencelist = new List<GameObject>();
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivateSentence();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateSentence()
+    {
+        index = Random.Range(0, Sentencelist.Count);
+        Sentencelist[index].SetActive(true);
+    }
+}
