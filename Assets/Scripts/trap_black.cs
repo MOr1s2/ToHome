1cc3cccf69418bfe0c5e01df9a852864635358a2
smart-grid2018 <46999312+smart-grid2018@users.noreply.github.com>
2020-07-20 16:39:59 +0800

    1，增加了夹子陷阱；2，修改了老鹰的碰撞体；

---
+++ Assets/Scripts/trap_black.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap_black : MonoBehaviour//控制夹子陷阱的脚本
+{
+    public GameObject trap;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            animator.SetBool("is_trigger", true);
+        }
+    }
+
+    public void to_static()
+    {
+        animator.SetBool("have_trigger", true);
+        Destroy(trap.gameObject, 2f);
+    }
+}
